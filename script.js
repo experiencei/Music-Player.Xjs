49c64f168959ab4a35311a49b7fa3eebcf288b7a
experiencei <ayelojahighbee01@gmail.com>
2021-08-20 12:05:26 +0100

    working on the functionality

--- script.js
+++ script.js
@@ -93,11 +93,28 @@ loadSong(songs[songIndex]);
 
 function updateProgressBar(e) {
     if (isPlaying) {
-      const { duration, currentTime } = e.srcElement;
-      // Update progress bar width
-      const progressPercent = (currentTime / duration) * 100;
-      progress.style.width = `${progressPercent}%`;
-    }
+        const { duration, currentTime } = e.srcElement;
+        // Update progress bar width
+        const progressPercent = (currentTime / duration) * 100;
+        progress.style.width = `${progressPercent}%`;
+        // Calculate display for duration
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if (durationSeconds < 10) {
+          durationSeconds = `0${durationSeconds}`;
+        }
+        // Delay switching duration Element to avoid NaN
+        if (durationSeconds) {
+          durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+        // Calculate display for currentTime
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if (currentSeconds < 10) {
+          currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+      }
 }
 
 // Event Listeners
